bf1e81df695f583047c816eeb6c8661dec62d592
Roza Getachew <getachewroza2@gmail.com>
2026-02-24 21:44:10 +0300

    added the chunker function which classify the trimmed text into chunkes using context, and similarities between them.

--- backend/rag/chuncker.py
+++ backend/rag/chuncker.py
@@ -3,3 +3,11 @@ def text_chunk(text: str, chunk_size=500, overlap=50):
     words = text.split()
     chunks = []
     start = 0
+
+    while start < len(words):
+        end = start + chunk_size
+        chunk = text[start:end]
+        chunks.append(chunk)
+        start += chunk_size - overlap
+
+    return chunks
